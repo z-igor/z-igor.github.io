8511a570bb8142cee15d2cc3cc7264a386214f61
Igor <gafgaf38@gmail.com>
2018-03-07 21:11:41 +0300

    Update script.js

--- gifs/script.js
+++ gifs/script.js
@@ -1,67 +1,66 @@
 (function() {
 
-'use strict';
+    'use strict';
 
-var videoSupport = !!document.createElement('video').canPlayType;
+    var videoSupport = !!document.createElement('video').canPlayType;
 
-if (videoSupport) {
-    var videoContainer = document.querySelectorAll('.grid-item');
+    if (videoSupport) {
+        var videoContainer = document.querySelectorAll('.grid-item');
+        var grid = document.querySelector('.grid');
 
-    var grid = document.querySelector('.grid');
-
-    var masonry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        columnWidth: 50,
-        percentPosition: true,
-        gutter: 50
-    });
-
-    /* var fireVideo = function(e) {
-         if (video.paused || video.ended)
-             video.play();
-         else
-             video.pause();
-     };*/
-
-    if (document.addEventListener) {
-        videoContainer.forEach(function(item) {
-             var span = document.createElement('span');
-            var video = item.querySelector('.video');
-            var playpause = item.querySelector('.playpause');
-
-            span.className = 'counts';
-            item.appendChild(span);
-
-            video.style.width = '200px';
-            video.controls = false;
-
-            playpause.addEventListener('mouseup', function(e) {
-                if (video.paused || video.ended)
-                    video.play();
-                else
-                    video.pause();
-            });
-
-            video.addEventListener('click', function(e) {
-                if (video.played) {
-                    playpause.style.opacity = 1;
-                }
-
-                if (e.which == 1) {
-                    if (video.paused || video.ended) {
-                        playpause.style.opacity = 0;
-                        video.play();
-                    } else {
-                        video.pause();
-                    }
-                }
-            });
-
+        var masonry = new Masonry(grid, {
+            itemSelector: '.grid-item',
+            columnWidth: 50,
+            percentPosition: true,
+            gutter: 50
         });
+
+        /* var fireVideo = function(e) {
+             if (video.paused || video.ended)
+                 video.play();
+             else
+                 video.pause();
+         };*/
+
+        if (document.addEventListener) {
+            videoContainer.forEach(function(item) {
+                var span = document.createElement('span');
+                var video = item.querySelector('.video');
+                var playpause = item.querySelector('.playpause');
+
+                span.className = 'counts';
+                item.appendChild(span);
+
+                video.style.width = '200px';
+                video.controls = false;
+
+                playpause.addEventListener('mouseup', function(e) {
+                    if (video.paused || video.ended)
+                        video.play();
+                    else
+                        video.pause();
+                });
+
+                video.addEventListener('click', function(e) {
+                    if (video.played) {
+                        playpause.style.opacity = 1;
+                    }
+
+                    if (e.which == 1) {
+                        if (video.paused || video.ended) {
+                            playpause.style.opacity = 0;
+                            video.play();
+                        } else {
+                            video.pause();
+                        }
+                    }
+                });
+            });
+        }
+
+        for (var i = 0; i < videoContainer.length; i++) {
+            videoContainer[i].querySelector('span').textContent = i + 1;
+        }
     }
-    for (var i = 0; i < videoContainer.length; i++) {
-        videoContainer[i].querySelector('span').textContent = i;
-    }
-}
 
 })();
