520c10aff944fa302a4c5648a0fad2999a3a21ed
Igor <gafgaf38@gmail.com>
2018-03-11 00:56:45 +0300

    Update script.js

--- gifs/script.js
+++ gifs/script.js
@@ -1,72 +1,69 @@
+'use strict';
+
 (function() {
-
-    'use strict';
-
     var videoSupport = !!document.createElement('video').canPlayType;
 
     if (videoSupport) {
         var videoContainer = document.querySelectorAll('.grid-item');
         var grid = document.querySelector('.grid');
+        var count = 0;
 
         var masonry = new Masonry(grid, {
             itemSelector: '.grid-item',
-            columnWidth: 20,
+            columnWidth: 0,
             percentPosition: true,
             gutter: 50
         });
 
-        /* var fireVideo = function(e) {
-             if (video.paused || video.ended)
-                 video.play();
-             else
-                 video.pause();
-         };*/
-
         if (document.addEventListener) {
             videoContainer.forEach(function(item) {
                 var span = document.createElement('span');
-                var video = item.querySelector('.video');
                 var playpause = item.querySelector('.playpause');
-
-                video.addEventListener('loadeddata', function() {
-
-                    if (video.readyState >= 2) {
-                        item.style.display = 'block';
-                    } else {
-                        item.style.display = 'none';
-                    }
-
-                });
+                var templateVideo = document.querySelector('#video__template').content;
+                var fragment = document.createDocumentFragment();
+                var lastVideo;
 
                 playpause.innerHTML = '&nbsp;<img src="player-play.png" alt="Play" draggable="false">';
                 span.className = 'counts';
+                span.textContent = ++count;
                 item.appendChild(span);
 
-                video.style.width = '200px';
-                video.controls = false;
+                item.style.backgroundImage = 'url("gifs/' + count + '.jpg")';
+                item.style.backgroundSize = 'contain';
 
-                item.addEventListener('mouseup', function(e) {
+                var createVideo = function() {
+                    var cloneVideo = templateVideo.cloneNode(true);
+                    var sourceVideo = cloneVideo.querySelector('source');
+                    sourceVideo.src = 'gifs/' + span.textContent + '.mp4';
+
+                    return cloneVideo;
+                }
+
+                item.addEventListener('click', function(evt) {
+                    if (!this.querySelector('.video')) {
+                        this.style.backgroundImage = 'url()';
+                        this.appendChild(createVideo());
+                    }
+
+                    var video = this.querySelector('video');
+
                     if (video.played) {
                         span.style.opacity = 1;
                         playpause.style.opacity = 1;
                     }
 
-                    if (e.which == 1) {
+                    if (evt.which == 1) {
                         if (video.paused || video.ended) {
                             playpause.style.opacity = 0;
-                            span.style.opacity = 0
+                            span.style.opacity = 0;
+
                             video.play();
                         } else {
                             video.pause();
                         }
                     }
-                });
+                })
             });
-        }
-
-        for (var i = 0; i < videoContainer.length; i++) {
-            videoContainer[i].querySelector('span').textContent = i + 1;
-            videoContainer[i].querySelector('video').poster = 'gifs/' + (i + 1) + '.jpg';
         }
     }
 
