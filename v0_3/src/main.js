4b09c81949321bf454858d09974e784e449603d1
z-igor <z-ig0rGITHUB>
2020-09-02 22:25:21 +0300

    add new files

--- v0_3/src/main.js
+++ v0_3/src/main.js
@@ -1,75 +1,16 @@
 import './styles/style.scss';
-import {
-  data,
-  covers
-} from '../../works';
 
-Vue.config.devtools = true;
+const wrapper = document.querySelector('.wrap');
+const cards = wrapper.querySelectorAll('.card');
 
-const app = new Vue({
-  el: '#app',
-  name: 'App',
-  data() {
-    return {
-      data: data.map(el => ({
-        ...el,
-        hover: false,
-        stylesImg: {},
-      })),
-      covers,
-      preloader: true,
-      isDestroy: null,
-    }
-  },
-  methods: {
-    onMouseOver(ev, card) {
-      let evt = ev.target;
-      let duration = null;
+cards.forEach(card => {
+    let stacks = card.querySelector('.stack');
 
-      card.hover = true;
+    card.addEventListener('mouseenter', (ev) => {
+        stacks.className = 'stack stack--show';
+    });
 
-      if (evt.className === 'img-item') {
-        if (evt.height >= 990) {
-          duration = 6;
-        }
-
-        if (evt.height <= 790) {
-          duration = 4
-        }
-
-        if (evt.height <= 590) {
-          duration = 3
-        }
-
-        if (evt.height <= 290) {
-          duration = 1
-        }
-
-        card.stylesImg = {
-          transitionDuration: duration + 's',
-          transform: `translate(0, -${evt.height - 200}px)`,
-        };
-      }
-    },
-    onMouseLeave(ev, card) {
-      let evt = ev.target;
-
-      card.hover = false;
-
-      if (evt.className === 'card') {
-        card.stylesImg = {
-          transitionDuration: '1s',
-          transform: `translate(0, 0)`,
-        }
-      }
-    },
-  },
-  mounted() {
-    this.isDestroy = setTimeout(() => {
-      this.preloader = false;
-    }, 500);
-  },
-  destroyed() {
-    this.isDestroy = null;
-  }
+    card.addEventListener('mouseleave', (ev) => {
+        stacks.className = 'stack stack--hide';
+    });
 });
