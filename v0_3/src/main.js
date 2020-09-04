b3f2c6b693ffeb85ee60824e5bf147acfb1fb04a
z-igor <z-ig0rGITHUB>
2020-09-05 02:39:03 +0300

    Setup webpack and created first page

--- v0_3/src/main.js
+++ v0_3/src/main.js
@@ -1,16 +1,47 @@
 import './styles/style.scss';
+import data from '../../works';
 
-const wrapper = document.querySelector('.wrap');
+Vue.config.devtools = true;
+
+const wrapper = document.getElementById('app');
 const cards = wrapper.querySelectorAll('.card');
 
 cards.forEach(card => {
-    let stacks = card.querySelector('.stack');
+  let stacks = card.querySelector('.stack');
 
-    card.addEventListener('mouseenter', (ev) => {
-        stacks.className = 'stack stack--show';
-    });
+  card.addEventListener('mouseenter', (ev) => {
+    stacks.className = 'stack stack--show';
+  });
 
-    card.addEventListener('mouseleave', (ev) => {
-        stacks.className = 'stack stack--hide';
-    });
+  card.addEventListener('mouseleave', (ev) => {
+    stacks.className = 'stack stack--hide';
+  });
+});
+
+const app = new Vue({
+  el: '#app',
+  name: 'App',
+  data() {
+    return {
+      data: data.map(el => ({
+        ...el,
+        hover: false,
+        images: [],
+      })),
+    }
+  },
+  methods: {
+    toggleMouseOverLeave(card, h) {
+      card.hover = h;
+    },
+    importAll(r) {
+      r.keys().forEach(key => (this.images.push({
+        pathLong: r(key),
+        pathShort: key
+      })));
+    },
+  },
+  mounted() {
+    this.importAll(require.context("../../works/blaz/", true, /\.jpg$/));
+  }
 });
