d8002b50834351062c1ab9de0f8f20bf86d1e6c4
z-igor <z-ig0rGITHUB>
2020-09-05 02:41:12 +0300

    Changed name from data.js to index.js into works file

---
+++ works/index.js
@@ -0,0 +1,44 @@
+export default [{
+  id: 'p1',
+    title: "Mini constructor",
+    description: "This mini constructor was developed via native javascript for view of first look",
+    source: "../../works/njs0_mini-constructor/index.html",
+    cover: "../../works/njs0_mini-constructor/mini-constructor.jpg",
+    coverBig: "",
+    offUrl: "https://vk.com/doc2000002202_440831900?hash=d587464e78ed55cf9f&dl=4b5e0dfee2ac6c566e",
+    stack: ['HTML', 'CSS', 'JS'],
+  },
+  {
+    id: 'p2',
+    title: "Creative+ A free Blog template web design",
+    description: "Today’s neat little web design template is Creative+ – A minimal blog design layout. This clean and minimal design is ready to take straight into WordPress or your CMS of choice. Download this free PSD Blog template web design psd and get blogging!",
+    source: "../works/creative_plus/index.html",
+    cover: "../works/creative_plus/this_300.jpg",
+    coverOrig: "../works/creative_plus/creative_orig.png",
+    offUrl: "http://blazrobar.com/free-psd-website-templates/creative-a-free-psd-blog-template-web-design/",
+    stack: ['HTML', 'CSS', 'JS'],
+  },
+  {
+    id: 'p3',
+    title: "title",
+    description: "description",
+    source: "source",
+    cover: "../works/dsgn/this_300.jpg",
+    coverOrig: "../works/dsgn/dsgn_orig_3000-7600.jpg",
+    offUrl: "http://theuncreativelab.com/portfolio/dsgn-free-psd-template/",
+    stack: ['HTML', 'CSS', 'JS'],
+  },
+];
+
+/* 
+{
+  id: "p4",
+  title: "demo",
+  description: "demo",
+  source: "demo",
+  cover: "demo",
+  coverOrig: "demo",
+  offUrl: "demo",
+  stack: ['HTML', 'CSS', 'JS'],
+} 
+*/
