6cc0aad7ed513bfb5e6b68b07ba92288222f1d99
z-igor <z-ig0rGITHUB>
2020-09-05 22:53:48 +0300

    Npm run dev - development - in /public

--- v0_3/webpack.config.js
+++ v0_3/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public')
   },
   devServer: {
     port: 3000
