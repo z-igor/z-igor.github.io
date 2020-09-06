6a9507b1fd1cd43ed876bc0d9689c127e174eeb4
z-igor <z-ig0rGITHUB>
2020-09-06 11:20:49 +0300

    Add vue.min.js for prodaction

--- v0_3/webpack.config.js
+++ v0_3/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     port: 3000
--- works/index.js
+++ works/index.js
@@ -1,17 +1,3 @@
-function importAllImages(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    let img = item.replace("./", '')
-      .replace(/\.(png|jpe?g|svg)$/, '');
-
-    images[img] = r(item).default;
-  });
-
-  return images;
-}
-
-export const covers = importAllImages(require.context('./asserts', false, /\.(png|jpe?g|svg)$/));
-
 export const data = [{
     id: 'p1',
     priority: 1,
@@ -19,7 +5,6 @@ export const data = [{
     description: "This mini constructor was developed via native javascript for view of first look",
     source: "../../works/njs0_mini-constructor/index.html",
     cover: '../../works/njs0_mini-constructor/mini-constructor.jpg',
-    _cover: 'njs0_mini-constructor',
     coverBig: "",
     offUrl: "https://vk.com/doc2000002202_440831900?hash=d587464e78ed55cf9f&dl=4b5e0dfee2ac6c566e",
     stack: ['HTML', 'CSS', 'JS'],
@@ -31,7 +16,6 @@ export const data = [{
     description: "Today’s neat little web design template is Creative+ – A minimal blog design layout. This clean and minimal design is ready to take straight into WordPress or your CMS of choice. Download this free PSD Blog template web design psd and get blogging!",
     source: "../../works/creative_plus/index.html",
     cover: "../../works/creative_plus/this_300.jpg",
-    _cover: "creative_plus",
     coverOrig: "../works/creative_plus/creative_orig.png",
     offUrl: "http://blazrobar.com/free-psd-website-templates/creative-a-free-psd-blog-template-web-design/",
     stack: ['HTML', 'CSS', 'JS'],
@@ -43,21 +27,8 @@ export const data = [{
     description: "DSGN - Free .PSD Template",
     source: "../../works/dsgn/index.html",
     cover: "../../works/dsgn/this_300.jpg",
-    _cover: "dsgn",
     coverOrig: "../works/dsgn/dsgn_orig_3000-7600.jpg",
     offUrl: "http://theuncreativelab.com/portfolio/dsgn-free-psd-template/",
     stack: ['HTML', 'CSS', 'JS'],
   },
 ];
-
-/* {
-  id: "p4",
-  title: "demo",
-  description: "demo",
-  source: "demo",
-  cover: "demo",
-  coverOrig: "demo",
-  offUrl: "demo",
-  stack: ['HTML', 'CSS', 'JS'],
-} 
-*/
