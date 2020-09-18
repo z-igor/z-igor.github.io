8aa1e0328ee56302148a3532dcec2fa4556c7eaa
z-igor <z-ig0rGITHUB>
2020-09-19 02:37:12 +0300

    Setup publication for trlogic

--- works/index.js
+++ works/index.js
@@ -1,4 +1,23 @@
 export const data = [{
+    id: 'p01',
+    priority: 1,
+    title: "ТЗ от TRLogic",
+    description: "Пет-проект",
+    source: "https://z-igor.github.io/",
+    cover: '../../works/trlogic/demo.png',
+    coverBig: "",
+    offUrl: "",
+    stack: ['Vue', 'HTML', 'CSS', 'JS'],
+    links: [{
+        github: "https://github.com/z-igor/z-igor.github.io/tree/master/works/trlogic/",
+      }, {
+        dribbble: "https://dribbble.com/shots/6609532-EverDo-Exploration/attachments",
+      }, {
+        "career.habr": "https://career.habr.com/companies/tr-logic",
+      }
+    ],
+  },
+  {
     id: 'p0',
     priority: 1,
     title: "Алексеич бригадир",
--- works/trlogic/vue.config.js
+++ works/trlogic/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/z-igor.github.io/works/trlogic/gh-pages' :
+    '/'
 };
