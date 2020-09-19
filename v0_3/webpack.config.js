19e527ce9b06edea65900133aa29bc8759013cbd
z-igor <z-igor@nomail.com>
2020-09-19 21:54:21 +0300

    Added local fonts

--- v0_3/webpack.config.js
+++ v0_3/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(woff(2)?|ttf|eot|png|jpe?g|gif)$/i,
         use: [{
           loader: 'file-loader',
         }, ],
